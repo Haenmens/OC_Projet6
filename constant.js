cae11be441f5bca0563e2f06a9e0e976b8b3041f
unknown <clamens.pierre@yahoo.fr>
2025-04-01 09:14:48 +0200

    Base de donnée terminée, retrait de l'adresse inutilisable

--- constant.js
+++ constant.js
@@ -2,7 +2,7 @@ const PORT_SERVEUR = 4000;
 
 const CLEF_JWT = "rytxryuizeg354qré(-(è-_èç_àç)à=:zefZEqf22013245";
 
-const ADRESSE_BDD = "mongodb+srv://test_user:test_pswd@cluster0.9kp91.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const ADRESSE_BDD = "";
 
 const ADRESSE_FRONT = "http://localhost:3000";
 
